6ae05cad124c4129f2c0d4549201717aa2fe631a
Tarun Kumar Kumar <mail2tkb@gmail.com>
2026-07-27 23:20:38 +0530

    Update config.js

--- Api/config/config.js
+++ Api/config/config.js
@@ -1,6 +1,6 @@
 module.exports = { 
     API_PORT				: "11801",  
-	WEB_PORT				: "11802",  
+	WEB_PORT				: "3000",  
 	ERROR_LOG               : ("log/error-%DATE%.log"), 
 	TOKEN_SECRET			: "$2a$10$gKfiGs.RqjCuX1zdxWGcUO", 
 	REMTOKEN_EXPIRY         : '7d',
